aff7e6c384fcd929ffbe93adbaa5b78fdbf99333
Adam Miller <kirov.sama@gmail.com>
2011-04-28 16:20:54 -0700

    Shared memory impl

--- src/cuda_ray_tracer.cu
+++ src/cuda_ray_tracer.cu
@@ -7,12 +7,12 @@
 
 using namespace RDST;
 
-__device__ float dot(vec3 v1, vec3 v2) {
+__device__ float dot(vec3 &v1, vec3 &v2) {
     return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
 }
 
 //Sphere Intersection
-__device__ float intersect(cuda_sphere_t sp, cuda_ray_t r) {
+__device__ float intersect(cuda_sphere_t &sp, cuda_ray_t &r) {
     //Intersection Code
     vec3 l;
     l.x = sp.c.x - r.o.x;
@@ -32,29 +32,51 @@ __device__ float intersect(cuda_sphere_t sp, cuda_ray_t r) {
     return t;
 }
 
-__global__ void RayTraceKernel(cuda_sphere_t spheres[], int spheresSize, cuda_ray_t rays[], cuda_intersection_t intrs[], int width, int height) {
+__global__ void RayTraceKernel(cuda_sphere_t spheres[], int spheresSize,
+        cuda_ray_t rays[], cuda_intersection_t intrs[], int width, int height) {
+    extern __shared__ cuda_sphere_t shared[];
+
+    if ((blockIdx.y * blockDim.y) >= height || (blockIdx.x * blockDim.x) >= width)
+        return;
+
     int row = threadIdx.y + blockIdx.y * blockDim.y;
     int col = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (row >= height || col >= width) {
-        return;
-    }
-
-    int idx = (row * width) + col;
 
     cuda_intersection_t inter;
     inter.objIndx = -1;
     inter.t = FLT_MAX;
 
-    for (int i = 0; i < spheresSize; i++) {
-        float newT = intersect(spheres[i], rays[idx]);
-        if (newT >= 0 && newT < inter.t) {
-            inter.t = newT;
-            inter.objIndx = i;
-        } 
+    int shPos = threadIdx.y * blockDim.x + threadIdx.x;
+
+    int count = spheresSize / (blockDim.x * blockDim.y);
+    int i = 0;
+    for (; i < count; i++) {
+        shared[shPos] = spheres[blockDim.x * blockDim.y * i + shPos];
+        __syncthreads();
+        for (int k = 0; k < blockDim.x * blockDim.y; k++) {
+            float newT = intersect(shared[k], rays[row * width + col]);
+            if (newT >= 0 && newT < inter.t) {
+                inter.t = newT;
+                inter.objIndx = i * blockDim.x * blockDim.y + k;
+            }
+        }
+        __syncthreads();
     }
 
-    intrs[idx] = inter;
+    int spill = spheresSize % (blockDim.x * blockDim.y);
+    if (shPos < spill)
+        shared[shPos] = spheres[blockDim.x * blockDim.y * i + shPos];
+    __syncthreads();
+    for (int k = 0; k < spill; k++) {
+        float newT = intersect(shared[k], rays[row * width + col]);
+        if (newT >= 0 && newT < inter.t) {
+            inter.t = newT;
+            inter.objIndx = i * blockDim.x * blockDim.y + k;
+        }
+    }
+
+    if (row < height && col < width)
+        intrs[row * width + col] = inter;
 }
 
 __host__ intersection_vec RDST::cuda_ray_trace(const sphere_vec &spheres, const ray_vec &rays, int width, int height) {
@@ -70,8 +92,8 @@ __host__ intersection_vec RDST::cuda_ray_trace(const sphere_vec &spheres, const 
     // Run kernel on spheres
     dim3 dimBlock(16, 16);
     dim3 dimGrid(512, 512);
-    RayTraceKernel<<<dimGrid, dimBlock>>>(sPtr, spheres.size(), rPtr, iPtr,
-            width, height);
+    RayTraceKernel<<<dimGrid, dimBlock, sizeof(cuda_sphere_t) * dimBlock.x * dimBlock.y>>>(sPtr,
+            spheres.size(), rPtr, iPtr, width, height);
 
     intersection_vec iVec(dIntersects.size());
     thrust::copy(dIntersects.begin(), dIntersects.end(), iVec.begin());
