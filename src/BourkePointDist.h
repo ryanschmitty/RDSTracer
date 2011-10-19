505322cab847eec33e9f8e8c4e27176b492c6ab8
Ryan Schmitt <schmitt.ryan@gmail.com>
2011-10-18 21:23:00 -0700

    Crazy new sphere surfel generation algorithm that works pretty well

--- src/BourkePointDist.h
+++ src/BourkePointDist.h
@@ -210,50 +210,48 @@ generateDistributedPoints(int numPoints, const RDST::Box& b, float* minDist)
 boost::shared_ptr< std::vector<glm::vec3> >
 generateDistributedPoints(int numPoints, const RDST::Sphere& sphere, int maxIterations, float* minDist)
 {
+    static const int MULTIPLIER = 5;
     int i,j,n;
-    int counter = 0,countmax = 100;
     int minp1,minp2;
     float r,d,mind,maxd;
-    glm::vec3 p[1000],p1,p2;
-    glm::mat4 xform = sphere.getModelXform();
+    glm::mat4 xform;
+    glm::vec3 c,p1,p2;
+    boost::shared_ptr< std::vector<glm::vec3> > p = boost::shared_ptr< std::vector<glm::vec3> >(new std::vector<glm::vec3>());
+    p->reserve(MULTIPLIER*n);
 
-    //Enforce minimums (and maximum)
-//    n = numPoints;
-//    r = sphere.getRadius();
-//    countmax = maxIterations;
-    if ((n = numPoints) < 2)
-        n = 3;
+    //Prep vars, enforce minimum points (and maximum)
+    if ((n = numPoints) < 10)
+        n = 10;
     else if (n > 1000)
         n = 1000;
-    if ((r = sphere.getRadius()) < 0.001)
-        r = 0.001f;
-    if ((countmax = maxIterations) < 100)
-        countmax = 100;
+    r = sphere.getRadius();
+    c = sphere.getCenter();
+    xform = sphere.getModelXform();
 
     /* Create the initial random cloud */
+    n = MULTIPLIER*n;
     int numU1s = (int)sqrtf((float)n);
     int numU2s = n / numU1s;
-    //Generate samples
     for (int i=0; i<numU1s; ++i) {
         for (int j=0; j<numU2s; ++j) {
             float u1 = (float)i/numU1s + RDST::unifRand()/numU1s;
             float u2 = (float)j/numU2s + RDST::unifRand()/numU2s;
             glm::vec3 v = sphere.uniformSample(u1, u2);
-            p[(i*numU2s) + j] = v;
+            p->push_back(v);
         }
     }
-    n = numU1s * numU2s;
+    n = numU1s * numU2s; //ensure accurate number
 
-    while (counter < countmax) {
+    while (p->size() > numPoints) {
 
-        /* Find the closest two points */
+        /* find the two closest points */
         minp1 = 0;
         minp2 = 1;
-        mind = Distance(p[minp1],p[minp2]);
+        mind = Distance((*p)[minp1],(*p)[minp2]);
         maxd = mind;
-        for (i=0;i<n-1;i++) {
-            for (j=i+1;j<n;j++) {
-                if ((d = Distance(p[i],p[j])) < mind) {
+        for (i=0;i<p->size()-1;i++) {
+            for (j=i+1;j<p->size();j++) {
+                if ((d = Distance((*p)[i],(*p)[j])) < mind) {
                     mind = d;
                     minp1 = i;
                     minp2 = j;
@@ -263,41 +261,17 @@ generateDistributedPoints(int numPoints, const RDST::Sphere& sphere, int maxIter
             }
         }
 
-        /*
-        Move the two minimal points apart, in this case by 1%
-        but should really vary this for refinement
-        */
-        p1 = p[minp1];
-        p2 = p[minp2];
-        p[minp2].x = p1.x + 1.05f * (p2.x - p1.x);
-        p[minp2].y = p1.y + 1.05f * (p2.y - p1.y);
-        p[minp2].z = p1.z + 1.05f * (p2.z - p1.z);
-        p[minp1].x = p1.x - 0.05f * (p2.x - p1.x);
-        p[minp1].y = p1.y - 0.05f * (p2.y - p1.y);
-        p[minp1].z = p1.z - 0.05f * (p2.z - p1.z);
-        Normalise(&p[minp1],r,sphere.getCenter());
-        Normalise(&p[minp2],r,sphere.getCenter());
-        p[minp1] = glm::vec3(xform * glm::vec4(p[minp1],1.f));
-        p[minp2] = glm::vec3(xform * glm::vec4(p[minp2],1.f));
-        if(Distance(p[minp1],p[minp2]) == 0.f) {
-            p[minp1] = sphere.uniformSample(RDST::unifRand(), RDST::unifRand());
-            p[minp2] = sphere.uniformSample(RDST::unifRand(), RDST::unifRand());
-        }
-
-        counter++;
+        //Remove one offending point
+        p->erase(p->begin()+minp1);
     }
-    *minDist = mind*2.f;
+
+    //Grab minimum distance
+    *minDist = 0.02f; //mind*2.f;
     printf("mindist: %f\n", mind);
     printf("maxdist: %f\n", maxd);
+    printf("numPoints: %u\n", (unsigned int)p->size());
 
-    /* Write out the points in your favorite format */
-    //Allocate memory (smart pointer to a vec3 array)
-    boost::shared_ptr< std::vector<glm::vec3> > pPointVec = boost::shared_ptr< std::vector<glm::vec3> >(new std::vector<glm::vec3>());
-    pPointVec->reserve(n);
-    for (int i=0; i<numPoints; ++i) {
-        pPointVec->push_back(p[i]);
-    }
-    return pPointVec;
+    return p;
 }
 
 void Normalise(glm::vec3* p, float r, glm::vec3 center)
